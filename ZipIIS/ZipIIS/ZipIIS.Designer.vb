5cbc5f887d161d415f150c65ed35174d2bdd9452
EllenTan <ellen.tan.k.w@hotmail.com>
2016-08-03 12:09:46 +0800

    Add project zip iis
    zip iis with vb.net in window form

---
+++ ZipIIS/ZipIIS/ZipIIS.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ZipIIS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StartZip = New System.Windows.Forms.Button()
+        Me.StopAndClose = New System.Windows.Forms.Button()
+        Me.txtIISSite = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtZipDirectory = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StartZip
+        '
+        Me.StartZip.Location = New System.Drawing.Point(402, 112)
+        Me.StartZip.Name = "StartZip"
+        Me.StartZip.Size = New System.Drawing.Size(75, 23)
+        Me.StartZip.TabIndex = 0
+        Me.StartZip.Text = "Start Zip"
+        Me.StartZip.UseVisualStyleBackColor = True
+        '
+        'StopAndClose
+        '
+        Me.StopAndClose.Location = New System.Drawing.Point(389, 181)
+        Me.StopAndClose.Name = "StopAndClose"
+        Me.StopAndClose.Size = New System.Drawing.Size(119, 23)
+        Me.StopAndClose.TabIndex = 1
+        Me.StopAndClose.Text = "Stop and close"
+        Me.StopAndClose.UseVisualStyleBackColor = True
+        '
+        'txtIISSite
+        '
+        Me.txtIISSite.Location = New System.Drawing.Point(131, 45)
+        Me.txtIISSite.Name = "txtIISSite"
+        Me.txtIISSite.Size = New System.Drawing.Size(346, 20)
+        Me.txtIISSite.TabIndex = 2
+        Me.txtIISSite.Text = "Default Web Site"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtZipDirectory)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtIISSite)
+        Me.GroupBox1.Controls.Add(Me.StartZip)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(496, 152)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Setting"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Zip to directory :"
+        '
+        'txtZipDirectory
+        '
+        Me.txtZipDirectory.Location = New System.Drawing.Point(131, 80)
+        Me.txtZipDirectory.Name = "txtZipDirectory"
+        Me.txtZipDirectory.Size = New System.Drawing.Size(346, 20)
+        Me.txtZipDirectory.TabIndex = 4
+        Me.txtZipDirectory.Text = "D:\Projects\testzip"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "IIS site :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label3.Location = New System.Drawing.Point(16, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(294, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Make sure that you have accessright to C:\inetpub\wwwroot"
+        '
+        'ZipIIS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(517, 222)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.StopAndClose)
+        Me.Name = "ZipIIS"
+        Me.Text = "IIS ZIP"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents StartZip As System.Windows.Forms.Button
+    Friend WithEvents StopAndClose As System.Windows.Forms.Button
+    Friend WithEvents txtIISSite As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtZipDirectory As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ ZipIIS/ZipIIS/ZipIIS.vb
@@ -0,0 +1,117 @@
+﻿Imports System.DirectoryServices
+Imports Microsoft.Web.Administration
+Imports System
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Text
+Imports System.Threading
+Imports ICSharpCode.SharpZipLib.Checksums
+Imports ICSharpCode.SharpZipLib.Zip
+Imports ICSharpCode.SharpZipLib.GZip
+Public Class ZipIIS
+
+    Private Sub StartZip_Click(sender As Object, e As EventArgs) Handles StartZip.Click
+        Dim defaultSite As Site = New ServerManager().Sites(txtIISSite.Text)
+        If (Not System.IO.Directory.Exists(txtZipDirectory.Text)) Then
+            System.IO.Directory.CreateDirectory(txtZipDirectory.Text)
+        End If
+        Dim filestream As New FileStream(txtZipDirectory.Text + "\log_ " + DateTime.Now.ToString("yyyy-MM-dd") + ".txt", FileMode.Create)
+        Dim streamwriter = New StreamWriter(filestream)
+        Dim writeFile As System.IO.TextWriter = New StreamWriter(filestream)
+        'streamwriter.AutoFlush = True
+        For Each app As Application In defaultSite.Applications
+            writeFile.WriteLine("{0} Found application with the following path: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), app.Path)
+            writeFile.WriteLine("Virtual Directories:")
+            If app.VirtualDirectories.Count > 0 Then
+                For Each vdir As VirtualDirectory In app.VirtualDirectories
+                    writeFile.WriteLine("Virtual Directory: {0}", vdir.Path)
+                    writeFile.WriteLine("   |-PhysicalPath = {0}", vdir.PhysicalPath)
+                    writeFile.WriteLine("   |-LogonMethod  = {0}", vdir.LogonMethod)
+                    writeFile.WriteLine("   +-UserName     = {0}\r\n", vdir.UserName)
+                    If app.Path.Trim.Replace("/", "") <> String.Empty Then
+                        Zipfolder(vdir.PhysicalPath, app.Path.Replace("/", ""), writeFile)
+                    End If
+                Next
+            End If
+        Next
+        Console.SetOut(writeFile)
+        Console.SetOut(streamwriter)
+        writeFile.Dispose()
+    End Sub
+
+    Sub Zipfolder(ByVal dir As String, ByVal dirname As String, ByVal writeFile As System.IO.TextWriter)
+        Dim strfilesaveto As String = txtZipDirectory.Text + "\" + dirname
+        Dim sTemp As String() = strfilesaveto.Split("\")
+        Dim sZipFileName As String = sTemp(sTemp.Length - 1).ToString()
+
+        ' check to see if zipped file already exists
+        Dim fi As FileInfo =
+        New FileInfo(txtZipDirectory.Text + "\" + dirname + ".zip")
+        If fi.Exists Then
+            Try
+                Dim sb As StringBuilder = New StringBuilder()
+                sb.Append("The directory " + dirname + " already exists. ")
+                sb.Append("Please delete the directory.")
+                MessageBox.Show(sb.ToString(), "Existing File Name")
+                writeFile.WriteLine("Existing File Name: {0}", sb.ToString())
+                Exit Sub
+            Catch ex As Exception
+                MessageBox.Show(ex.Message, "File Error")
+                writeFile.WriteLine("File Error: {0}", ex.ToString())
+                Exit Sub
+            End Try
+        End If
+        fi = Nothing
+
+
+        ' Check for the existence of the target folder and
+        ' create it if it does not exist
+        If (Not System.IO.Directory.Exists(strfilesaveto + "\TempZipFile\")) Then
+            System.IO.Directory.CreateDirectory(strfilesaveto + "\TempZipFile\")
+        End If
+
+        ' Set up a string to hold the path to the temp folder
+        Dim sTargetFolderPath As String = (strfilesaveto + "\TempZipFile\")
+        ' zip up the files
+        Try
+
+            Dim filenames As String() = Directory.GetFiles(dir)
+            ' Zip up the files - From SharpZipLib Demo Code
+            Dim s As ZipOutputStream = New ZipOutputStream(File.Create(txtZipDirectory.Text + "\" + dirname + ".zip"))
+            s.SetLevel(9) ' 0-9, 9 being the highest level of compression
+            Dim buffer() As Byte
+            ReDim buffer(4096)
+            Dim f As String
+            For Each f In filenames
+                Dim entry As ZipEntry = New ZipEntry(Path.GetFileName(f))
+                entry.DateTime = DateTime.Now
+                s.PutNextEntry(entry)
+                Dim fs As FileStream = File.OpenRead(f)
+                Dim sourceBytes As Integer = 1
+
+                Do Until (sourceBytes <= 0)
+                    sourceBytes = fs.Read(buffer, 0, buffer.Length)
+                    s.Write(buffer, 0, sourceBytes)
+                Loop
+                fs.Close()
+            Next
+            s.Finish()
+            s.Close()
+            MessageBox.Show("Zip file " + strfilesaveto + " created.")
+            writeFile.WriteLine("Zip file: {0} created", strfilesaveto)
+            ' clean up files by deleting the temp folder and its content
+            System.IO.Directory.Delete(strfilesaveto + "\TempZipFile\", True)
+
+        Catch ex As Exception
+            System.IO.Directory.Delete(strfilesaveto + "\TempZipFile\", True)
+            MessageBox.Show(ex.Message.ToString(), "Zip Operation Error")
+            writeFile.WriteLine("Zip Operation Error: {0}", ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub StopAndClose_Click(sender As Object, e As EventArgs) Handles StopAndClose.Click
+        Me.Close()
+    End Sub
+
+
+End Class
